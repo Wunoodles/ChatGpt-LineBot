2333ab483cb84fb2d63a184a6031c9b340990528
noodles <noodlesloves@gmail.com>
2023-02-22 23:13:49 +0800

    'edit'

--- api/index.py
+++ api/index.py
@@ -43,14 +43,12 @@ def handle_message(event):
 
 
     if event.message.text == '--help':
-        text = """
-            圖片生成指令: /圖片:[prompt]
-            程式生成指令: /產生程式:[language]/[prompt]
-            文法校正指令: /英文校正:[prompt]
-            蒐集資料: /收集:[數量]/[領域]
-            內容總結: /總結:[數量]/[內容]
-            chat_gpt: 輸入 "/啟動" 後開始聊天，輸入 "/結束" 結束聊天
-        """
+        text = """圖片生成指令: /圖片:[prompt]
+程式生成指令: /產生程式:[language]/[prompt]
+文法校正指令: /英文校正:[prompt]
+蒐集資料: /收集:[數量]/[領域]
+內容總結: /總結:[數量]/[內容]
+chat_gpt: 輸入 "/啟動" 後開始聊天，輸入 "/結束" 結束聊天"""
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=text))
