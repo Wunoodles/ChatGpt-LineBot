2a1edb558f33f739a453ebea00e345dd6b7fcdd9
noodles <noodlesloves@gmail.com>
2023-02-22 23:08:33 +0800

    'add_more_prompt_example'

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -2,8 +2,9 @@ from api.prompt import Prompt
 
 import os
 import openai
+from const import *
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
 
 class ChatGPT:
     def __init__(self):
@@ -13,7 +14,7 @@ class ChatGPT:
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 2000))
 
     def get_response(self):
         response = openai.Completion.create(
@@ -25,6 +26,68 @@ class ChatGPT:
             max_tokens=self.max_tokens
         )
         return response['choices'][0]['text'].strip()
+
+    def get_image(self, prompt):
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="1024x1024"
+        )
+
+        return response['data'][0]['url']
+
+
+    def get_grammer_check(self, prompt):
+        start_prompt = 'Can you check the spelling and grammar in the following text? '
+        response = openai.Completion.create(
+            model=self.model,
+            prompt=start_prompt+prompt,
+            temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            max_tokens=self.max_tokens
+        )
+        return response['choices'][0]['text'].strip()
+
+    
+    def get_code(self, language, prompt):
+        prompt_ = f'你現在是一個 {language} 專家，請幫我用 {language} 寫一個函式，它需要做到 {prompt} 某個功能'
+        response = openai.Completion.create(
+            model=self.model,
+            prompt=prompt_,
+            temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            max_tokens=self.max_tokens
+        )
+        return response['choices'][0]['text'].strip()
+
+    
+    def get_collect_domain_paper(self, count, prompt):
+        prompt_ = f'給我 f{count} 篇，有關 {prompt} 的文章'
+        response = openai.Completion.create(
+            model=self.model,
+            prompt=prompt_,
+            temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            max_tokens=self.max_tokens
+        )
+        return response['choices'][0]['text'].strip()
+
+
+    def get_coutent_summary(self, count, prompt):
+        prompt_ = f'用列點的方式總結出這篇文章的 {count} 個重點：{prompt}'
+        response = openai.Completion.create(
+            model=self.model,
+            prompt=prompt_,
+            temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            max_tokens=self.max_tokens
+        )
+        return response['choices'][0]['text'].strip()
+
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
--- api/index.py
+++ api/index.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage 
 from api.chatgpt import ChatGPT
 
-import os
+import os, re
+from const import *
 
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
@@ -39,19 +40,74 @@ def handle_message(event):
     
     if event.message.type != "text":
         return
+
+
+    if event.message.text == '--help':
+        text = """
+            圖片生成指令: /圖片:[prompt]
+            程式生成指令: /產生程式:[language]/[prompt]
+            文法校正指令: /英文校正:[prompt]
+            蒐集資料: /收集:[數量]/[領域]
+            內容總結: /總結:[數量]/[內容]
+            chat_gpt: 輸入 "/啟動" 後開始聊天，輸入 "/結束" 結束聊天
+        """
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=text))
+
+
+    if event.message.text.startswith('/圖片:'):
+        prompt = event.message.text.replace('/圖片:')
+        image_url = chatgpt.get_image(prompt)
+        line_bot_api.reply_message(
+            event.reply_token,
+            ImageSendMessage(original_content_url=image_url,
+                             preview_image_url=image_url))
     
-    if event.message.text == "啟動":
+
+    if event.message.text.startswith('/英文校正:'):
+        prompt = event.message.text.replace('/英文校正:')
+        reply_msg = chatgpt.get_grammer_check(prompt)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
+        
+    
+    if event.message.text.startswith('/產生程式:'):
+        lan, prompt = event.message.text.replace('/產生程式:').split('/')
+        reply_msg = chatgpt.get_code(lan, prompt)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
+
+
+    if event.message.text.startswith('/收集:'):
+        count, prompt = event.message.text.replace('/收集:').split('/')
+        reply_msg = chatgpt.get_collect_domain_paper(count, prompt)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
+
+
+    if event.message.text.startswith('/總結:'):
+        count, prompt = event.message.text.replace('/總結:').split('/')
+        reply_msg = chatgpt.get_coutent_summary(count, prompt)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
+
+    if event.message.text == "/啟動":
         working_status = True
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
+            TextSendMessage(text="開始聊天~"))
         return
 
-    if event.message.text == "安靜":
+    if event.message.text == "/結束":
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
+            TextSendMessage(text="下次再見~"))
         return
     
     if working_status:
